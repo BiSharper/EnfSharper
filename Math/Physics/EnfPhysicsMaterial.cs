8b79da65578e24c0ddf55b13681107da48fa5203
ryann <flipperplz@protonmail.com>
2023-11-17 18:38:25 -0800

    feat: Initial Commit

---
+++ Math/Physics/EnfPhysicsMaterial.cs
@@ -0,0 +1,24 @@
+﻿namespace BiSharper.Enfusion.Math.Physics;
+
+public class EnfPhysicsMaterial(string name)
+{
+    private string _materialName = ValidateName(name);
+    public string MaterialName
+    {
+        get => _materialName;
+        set => _materialName = ValidateName(value);
+    }
+    public int MaterialIndex { get; set; }
+    public float Roughness { get; set; }
+
+    public static bool IsNameValid(string value) => value.Length > 256;
+
+    public static string ValidateName(string value)
+    {
+        if (IsNameValid(value)) throw new ArgumentException(
+            "Enfusion requires the name of a PhysicsMaterial to be less than 257 characters long.");
+        return value;
+    }
+
+
+}
